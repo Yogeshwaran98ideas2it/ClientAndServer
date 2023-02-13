bad4c620168f14462e84589649bfe54e01cbdfed
Yogeshwaran <yogeshwaran.mohan@ideas2it.com>
2023-02-13 15:08:16 +0530

    config file changed

--- healthcare/src/main/java/com/example/healthcare/HealthcareApplication.java
+++ healthcare/src/main/java/com/example/healthcare/HealthcareApplication.java
@@ -2,6 +2,7 @@ package com.example.healthcare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
@@ -14,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  */
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableConfigServer
 public class HealthcareApplication {
 
 	/**
