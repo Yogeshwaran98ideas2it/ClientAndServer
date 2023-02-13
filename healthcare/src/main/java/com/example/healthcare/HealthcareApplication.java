f3f190488f695c8eea824d104a693a8214c77981
Yogeshwaran <yogeshwaran.mohan@ideas2it.com>
2023-02-13 15:16:18 +0530

    pom.xml changed

--- healthcare/src/main/java/com/example/healthcare/HealthcareApplication.java
+++ healthcare/src/main/java/com/example/healthcare/HealthcareApplication.java
@@ -2,7 +2,6 @@ package com.example.healthcare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
